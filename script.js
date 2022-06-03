c71279c6932ffebe4b5eec037954e4f763c08f83
Dante <guchoo923@gmail.com>
2022-06-03 21:37:50 +0300

    Add functionality to social buttons, change classes

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const navLinks = Array.from(document.querySelectorAll('nav a'));
+// const navLinks = Array.from(document.querySelectorAll('nav a'));
 
-navLinks.forEach((link)=>{
-    link.addEventListener('click',()=>{
-        navLinks.forEach((link)=>{ 
-            if(link.classList.contains('.active'))
-    })
-    })
+// navLinks.forEach((link)=>{
+//     link.addEventListener('click',()=>{
+//         navLinks.forEach((link)=>{ 
+//             if(link.classList.contains('.active'))
+//     })
+//     })
+// })
+document.querySelector(".github").addEventListener('click', () => {
+    window.open('https://github.com/dante2302', '_blank');
+})
+document.querySelector('.linkedin').addEventListener('click', ()=>{
+    window.open('https://www.linkedin.com/in/darin-penchev-1aa74b237/', '_blank');
 })
