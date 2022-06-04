a2d96a79a5fb548d74ae8e0694c2727e2691cda2
Dante <guchoo923@gmail.com>
2022-06-04 17:14:53 +0300

    Create project-card template, add functionality

--- script.js
+++ script.js
@@ -7,9 +7,20 @@
 //     })
 //     })
 // })
-document.querySelector(".github").addEventListener('click', () => {
-    window.open('https://github.com/dante2302', '_blank');
-})
-document.querySelector('.linkedin').addEventListener('click', ()=>{
-    window.open('https://www.linkedin.com/in/darin-penchev-1aa74b237/', '_blank');
-})
+    document.querySelector(".github").addEventListener('click', () => {
+        window.open('https://github.com/dante2302', '_blank');
+    })
+    
+    document.querySelector('.linkedin').addEventListener('click', ()=>{
+        window.open('https://www.linkedin.com/in/darin-penchev-1aa74b237/', '_blank');
+    })
+
+    document.querySelectorAll('.card').forEach((card)=>{
+      card.addEventListener('click',()=>{
+            card.classList.toggle('card-active');
+            if(card.classList.contains('card-active')){
+                card.style.transform="rotateY(180deg)";
+            }
+            else card.style.transform="rotateY(360deg)";
+        })
+    })
