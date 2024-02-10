5764fbc583c29b2234e9570b0b53b2780ebf4bb6
Dante <guchoo923@gmail.com>
2024-02-10 20:18:56 +0200

    Fully style navbar

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,9 +1,24 @@
 import NavButton from "./NavButton";
+import { useState, useEffect } from 'react';
 import "./Nav.css";
 
 export default function Nav() {
+    const SCROLL_TOP_BOUNDARY = 320;
+    const [scrollTop, setScrollTop] = useState(0);
+
+    useEffect(() => {
+      const handleScroll = (event: Event) => {
+        setScrollTop(window.scrollY);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
     return (
-        <nav>
+        <nav className={scrollTop > SCROLL_TOP_BOUNDARY ? "bg-white" : ""}>
            <NavButton 
                 styleClass="nav-button"
                 content="Home"
