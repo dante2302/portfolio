6ec0cb2c01ee16bcedbabbf99fe874b7b6874912
Dante <guchoo923@gmail.com>
2024-02-11 18:17:41 +0200

    Add svgs, links and cv

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,6 +1,10 @@
 import NavButton from "./NavButton";
 import { useState, useEffect } from 'react';
 import "./Nav.css";
+import githubLogo from "../../assets/github.svg";
+import linkedInLogo from "../../assets/Linkedin_logo.svg";
+import resumeLogo from "../../assets/pdf-round.svg";
+import resume from "../../assets/cv.pdf";
 
 export default function Nav() {
     const SCROLL_TOP_BOUNDARY = 320;
@@ -18,27 +22,30 @@ export default function Nav() {
       };
     }, []);
     return (
-        <nav className={scrollTop > SCROLL_TOP_BOUNDARY ? "bg-white" : ""}>
-           <NavButton 
-                styleClass="nav-button"
-                content="Home"
-                navigateTo="#"
-            />
-           <NavButton 
-                styleClass="nav-button"
-                content="About"
-                navigateTo="#About"
-            />
-           <NavButton 
-                styleClass="nav-button"
-                content="Projects"
-                navigateTo="#Projects"
-            />
-           <NavButton 
-                styleClass="nav-button"
-                content="Contact"
-                navigateTo="#Contact"
-            />
-        </nav>
+      <nav className={scrollTop > SCROLL_TOP_BOUNDARY ? "bg-white" : ""}>
+        <NavButton
+          styleClass="nav-button"
+          content="Home"
+          navigateTo="#"
+        />
+        <NavButton
+          styleClass="nav-button"
+          content="About"
+          navigateTo="#About"
+        />
+        <NavButton
+          styleClass="nav-button"
+          content="Projects"
+          navigateTo="#Projects"
+        />
+        <NavButton
+          styleClass="nav-button"
+          content="Contact"
+          navigateTo="#Contact"
+        />
+        <a className="logo-link" href="https://github.com/dante2302" target="_blank"><img src={githubLogo} className="nav-logo" /></a>
+        <a className="logo-link" href="https://www.linkedin.com/in/darinpenchev/" target="_blank"><img src={linkedInLogo} className="nav-logo" /></a>
+        <a className="logo-link" href={resume} target="_blank"><img src={resumeLogo} className="nav-logo adjusted-resume-logo"/></a>
+      </nav>
     )
 }
