3e7df79f4c9301d8603fdb5974ad16deb45c0bf4
Dante <guchoo923@gmail.com>
2024-07-26 18:01:54 +0300

    fix nav links

--- frontend/src/Components/Nav/NavBar.tsx
+++ frontend/src/Components/Nav/NavBar.tsx
@@ -2,15 +2,29 @@ import githubLogo from "../../assets/github.svg";
 import linkedInLogo from "../../assets/Linkedin_logo.svg";
 import resumeLogo from "../../assets/pdf-round.svg";
 import resume from "../../assets/cv.pdf";
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 
-interface props extends PropsWithChildren{
-    styleClass: string,
-    closeModal?: () => void,
+interface props extends PropsWithChildren {
+  styleClass: string,
+  closeModal?: () => void,
 }
 
 export default function NavBar({ styleClass, children, closeModal }: props) {
-  useEffect(() => {console.log("A")})
+  const [activeSection, setActiveSection] = useState("");
+
+  const handleLinkClick = (sectionId: string) => {
+    setActiveSection(sectionId);
+    closeModal && closeModal();
+  };
+
+  useEffect(() => {
+    if (activeSection) {
+      const element = document.querySelector(activeSection);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [activeSection]);
 
   return (
     <nav className={styleClass || `nav-bar`}>
@@ -18,11 +32,11 @@ export default function NavBar({ styleClass, children, closeModal }: props) {
       <div className="internal-link-container">
         <a className="nav-button" onClick={() => {
           closeModal && closeModal();
-          window.scrollTo({top: 0});
+          window.scrollTo({ top: 0 });
         }}>Home</a>
-        <a className="nav-button" href="#About" onClick={() => closeModal && closeModal()}>About</a>
-        <a className="nav-button" href="#Projects" onClick={() => closeModal && closeModal()}>Projects</a>
-        <a className="nav-button" href="#Contact" onClick={() => closeModal && closeModal()}>Contact</a>
+        <a className="nav-button" href="#About" onClick={() => handleLinkClick("#About")}>About</a>
+        <a className="nav-button" href="#Projects" onClick={() => handleLinkClick("#Projects")}>Projects</a>
+        <a className="nav-button" href="#Contact" onClick={() => handleLinkClick("#Contact")}>Contact</a>
       </div>
       <div>
         <a className="logo-link" href="https://github.com/dante2302" target="_blank"><img src={githubLogo} className="nav-logo" /></a>
